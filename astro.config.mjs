0b5ab3d697886f775c9579eacd07570948453eae
imperionite <lr.aimperial@mmdc.mcl.edu.ph>
2025-07-03 01:54:45 +0800

    :recycle: (src) edit files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: "https://ghgph-55623.firebaseapp.com",
-    outDir: "dist",
+  site: 'https://ghgph-55623.firebaseapp.com',
+  outDir: 'dist',
+  vite: {
+    server: {
+      hmr: true, 
+      watch: {
+        usePolling: true, 
+        interval: 100,    
+      },
+      host: '0.0.0.0', 
+      port: 4321      
+    }
+  }
 });
